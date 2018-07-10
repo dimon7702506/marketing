01872e84430517f8472a56e311b4c333f1471560
Dmitriy <7702506@gmail.com>
2018-07-10 21:39:07 +0300

    20180710_1

--- send.php
+++ send.php
@@ -10,8 +10,9 @@ $mail->Password = 'Ebuprofen';
 //$mail->SMTPSecure = ‘ssl’;
 $mail->Port = 587;
 $mail->setFrom('admin@falbi.kh.ua');
-$mail->addAddress('dimon@falbi.kh.ua');
-$mail->addAddress("synhro@falbi.kh.ua"); // Еще один email, если нужно.
+//$mail->addAddress('dimon@falbi.kh.ua');
+$mail->addAddress("synhro@falbi.kh.ua");
+$mail->addAddress("synhro@passat.kh.ua");
 // Прикрепление файлов
 $mail->addAttachment('/var/www/marketing.com/out/names.csv', 'names.csv');
 $mail->addAttachment('/var/www/marketing.com/out/marketings.csv', 'marketings.csv');
@@ -22,7 +23,7 @@ $mail->Subject = 'Synhronization'; // Заголовок письма
 $mail->Body = 'Synhronization names and marketings'; // Текст письма
 // Результат
 if(!$mail->send()) {
-    echo "<h1 style='color: red'> Message could not be sent.</h1>";
+    echo "<h1 style='color: red'>Message could not be sent.</h1>";
     echo "<h3>Mailer Error: . $mail->ErrorInfo </h3>";
 } else {
     header("location: admin.html");
