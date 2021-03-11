78e3dcc16a2c5c9324cfa377049a61b6537bc94e
Dmitriy <7702506@gmail.com>
2021-03-11 17:10:38 +0200

    20210311

--- function.php
+++ function.php
@@ -2,6 +2,18 @@
 
 require_once "./autoload.php";
 
+function super_unique($array,$key)
+{
+    $temp_array = [];
+    foreach ($array as &$v) {
+        if (!isset($temp_array[$v[$key]]))
+            $temp_array[$v[$key]] =& $v;
+    }
+    $array = array_values($temp_array);
+    return $array;
+
+}
+
 function generateCode($length=6) {
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
     $code = "";
--- menu.php
+++ menu.php
@@ -55,8 +55,10 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Загрузить')
 }
 $_SESSION['message'] = $message;
 
-foreach ($result as $key=>$value){
-    if ($key == 0){
+$k = 0;
+
+foreach ($result as $key=>$value) {
+    if ($key == 0) {
         continue;
     }
     //var_dump($value);
@@ -64,21 +66,60 @@ foreach ($result as $key=>$value){
     $results = $find->result_data;
     //var_dump($results);
     $email = $results[0]['email'];
+    $apt_name = $value[5];
+    $apt_name = str_replace("~", "", $apt_name);
 
-    $apt = ['apteka_id'=> $value[12],
-            'apteka_name'=> $value[5],
-            'name'=> $value[0],
-            'quantity'=>$value[4],
-            'email'=>$email,
-            'sender'=>get_user_email()];
+    $apt[$k] = ['apteka_id' => $value[12],
+        'apteka_name' => $apt_name,
+        'name' => $value[0],
+        'quantity' => $value[4],
+        'email' => $email];
 
-    $emails = $apt;
+    $emails[$k] = ['email' => $email,
+                   'apteka_name' => $apt_name];
+    $k += 1;
+}
+    //var_dump($emails);
 
-    unset($emails['apteka_id']);
-    unset($emails['apteka_name']);
-    unset($emails['name']);
-    unset($emails['quantity']);
-    unset($emails['sender']);
-    $emails_uniq = array_unique($emails);
-    var_dump($emails_uniq);
+    $emails_uniq = super_unique($emails,'email');
+    //var_dump($emails_uniq);
+
+foreach ($emails_uniq as $key => $e){
+    //var_dump($e);
+
+    $text = '<br>' . '<br>' . $e['apteka_name'].":";
+    foreach ($apt as $a){
+        if ($a['email'] !== $e['email']) {continue;}
+        $text .= '<br>'.$a['name'] . "  - " . $a['quantity'];
+    }
+
+    $text_1251 = iconv("UTF-8", "Windows-1251", $text);
+
+    $sender = get_user_email();
+    $sender = 'olya@passat.kh.ua';
+
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'mx1.mirohost.net';
+    $mail->SMTPAuth = true;
+    $mail->Username = $sender;
+    $mail->Password = 'Ebuprofen';
+    //$mail->SMTPSecure = false;
+    $mail->SMTPOptions = array('ssl' => array('verify_peer' => false,'verify_peer_name' => false,'allow_self_signed' => true));
+    $mail->Port = 587;
+    $mail->setFrom($sender);
+    //$mail->addAddress($e['email']);
+    $mail->addAddress($sender);
+    // Письмо
+    $mail->CharSet = 'Windows-1251';
+    $mail->isHTML(true);
+    $mail->Subject = 'Order'; // Заголовок письма
+    $mail->Body = $text_1251; // Текст письма
+
+    echo $text;
+    // Результат
+    if(!$mail->send()) {
+        echo "<h1 style='color: red'>Message could not be sent.</h1>";
+        echo "<h3>Mailer Error: . $mail->ErrorInfo </h3>";
+    }
 }
